886db91fcd8fee3049db8123c6560c48d4ab01c3
Salah Khattab <salah.khat96@gmail.com>
2025-12-29 20:56:53 +0300

    feat(cleanup): remove deprecated API handler and Vercel configuration files

--- backend/api.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from wsgi_adapter import handler as wsgi_handler
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beebol_backend.settings')
-
-app = get_wsgi_application()
-handler = wsgi_handler(app)
